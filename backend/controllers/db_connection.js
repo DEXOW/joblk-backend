221d2f9a2cf3d78f0641c85902e0d5783cd94c23
Thinal Perera <thinaltp@gmail.com>
2023-11-22 12:11:29 +0530

    using mysql2 instead of mysql, changed variable names

--- backend/controllers/db_connection.js
+++ backend/controllers/db_connection.js
@@ -3,8 +3,8 @@ const db = require('mysql');
 const connection = db.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME
 });
 
